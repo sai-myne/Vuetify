d18510ab7f7c3de032f5d08929a114e923d7ef25
sai-myne <ahlusir@gmail.com>
2020-07-14 14:09:28 +0800

    Cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,8 +2,26 @@
   <div class="team">
     <h1 class='subtitle-1 grey--text mx-4 mb-4'>Team</h1>
 
-    <v-container fluid class="my-5">
-        content
+    <v-container class="my-5">
+        <v-layout row wrap>
+            <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                <v-card flat class="text-center ma-3">
+                    <v-responsive class="pt-4">
+                        Image gose here
+                    </v-responsive>
+                    <v-card-text>
+                        <div class="subtitle-1">{{ person.name }}</div>
+                        <div class="grey--text">{{ person.role }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn text color="grey">
+                            <v-icon small left>mdi-message</v-icon>
+                            <span>Message</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </v-container>
     
   </div>
@@ -13,6 +31,16 @@
 // @ is an alias to /src
 
 export default {
-
+    data(){
+        return{
+            team: [
+                { name: 'The Net Ninja', role: 'Web developer' },
+                { name: 'Ryu', role: 'Graphic designer' },
+                { name: 'Chun Li', role: 'Web developer' },
+                { name: 'Gouken', role: 'Social media maverick' },
+                { name: 'Yoshi', role: 'Sales guru'}
+            ]
+        }
+    }
 }
 </script>
