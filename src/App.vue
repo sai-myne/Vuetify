fe2ab1aee55e2688b45352c2e79d92a64edb6f44
sai-myne <ahlusir@gmail.com>
2020-07-14 12:01:29 +0800

    Toolbars

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app>
-    
-    <v-main>
+    <Navbar />
+    <v-main class="grey lighten-4">
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
 
+  components: { Navbar },
   
 
   data: () => ({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="home">
     <h1>homepage</h1>
-    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
-
-    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
-
-    <v-btn class="hidden-sm-only">click me</v-btn>
+    
   </div>
 </template>
 
