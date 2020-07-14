621ce2a5d5dc6395cb45b11d6122a24b075b748d
sai-myne <ahlusir@gmail.com>
2020-07-14 13:21:40 +0800

    The Grid System Part 1

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,8 +2,32 @@
   <div class="dashboard">
     <h1 class='subtitle-1 grey--text mx-4 mb-4'>Dashboard</h1>
     
-    <v-container fluid class="my-5">
-        content
+    <v-container class="my-5">
+        
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outlined block color="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outlined block color="success">2</v-btn>
+        </v-flex>
+      </v-layout>
+
     </v-container>
   </div>
 </template>
