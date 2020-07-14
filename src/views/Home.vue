0c9ada640c52130af91fa050d99cfde3feeb44be
sai-myne <ahlusir@gmail.com>
2020-07-14 10:46:26 +0800

    Text & Color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam aut delectus necessitatibus, rem et officia voluptatibus odit excepturi illum dolore quae sequi doloribus voluptate exercitationem neque aspernatur tenetur molestias possimus.</p>
+    <p class='pink lighten-4 red--text text--darken-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium est, laboriosam perspiciatis ducimus iste nam optio harum itaque sunt, quod eos numquam corrupti nulla culpa odit cum libero voluptates impedit!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class='display-1'>Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subtitle-1 font-weight-bold">This is a subheading</p>
+    <p class="caption">this is a caption</p>
   </div>
 </template>
 
@@ -8,6 +15,6 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+
 }
 </script>
