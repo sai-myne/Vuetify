ad853926070935da57bdf3ae905d8379e7183669
sai-myne <ahlusir@gmail.com>
2020-07-14 14:42:51 +0800

    Popup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,6 +38,9 @@
                         The Net Ninja
                     </p>
                 </v-flex>
+                <v-flex class="mt-4 mb-3">
+                    <Popup />
+                </v-flex>
             </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -55,7 +58,9 @@
 
 
 <script>
+import Popup from './Popup'
 export default {
+    components: { Popup },
     data(){
         return{
             drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-dialog max-width="600px">
+        <template v-slot:activator="{on}">
+            <v-btn text class="success" v-on="on">Add new project</v-btn>
+        </template>        
+        <v-card>
+            <v-card-title>
+                <h2>Add a New project</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
