7ffe91d256194c657462799e956e6980aa1d626e
sai-myne <ahlusir@gmail.com>
2020-07-14 14:17:23 +0800

    Avatars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,6 +13,16 @@
             </v-btn>
         </v-app-bar>
         <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                    <p class="white--text subtitle-1 mt-1">
+                        The Net Ninja
+                    </p>
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
